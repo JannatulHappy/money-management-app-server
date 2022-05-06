cae4a3282e04896787c4a0f0b76135449711afb6
JannatulHappy <jannatulferdoushappy21@gmail.com>
2022-05-06 16:24:23 +0600

    error

--- controller/userController.js
+++ controller/userController.js
@@ -1,4 +1,6 @@
+const bcrypt = require("bcrypt");
 const registerValidator = require("../validator/registerValidator");
+const User = require("../model/User");
 // login controller
 module.exports = {
   login(req, res) {
@@ -21,9 +23,50 @@ module.exports = {
     if (!validate.isValid) {
       res.status(400).json(validate.error);
     } else {
-      res.status(200).json({
-        message: "Everything is Okay!!",
-      });
+      User.findOne({ email });
+      console
+        .log(email)
+        .then((user) => {
+          if (user) {
+            return res.status(400).json({
+              message: "User is Already exist",
+            });
+          }
+
+          bcrypt.hash(password, 11, (err, hash) => {
+            if (err) {
+              return res.status(500).json({
+                message: "Server Error Occurred",
+              });
+            }
+            let user = new User({
+              name,
+              email,
+              password: hash,
+            });
+            res.json(user);
+            user
+              .save()
+              .then(user=>{
+                res.status(201).json({
+                  message:"User created successfully",
+                  user
+                })
+              })
+              .catch((error) => {
+                console.log(error);
+                res.status(500).json({
+                  message: "Server error occurred",
+                });
+              });
+          });
+        })
+        .catch((error) => {
+          console.log(error);
+          res.status(500).json({
+            message: "Server error occurred",
+          });
+        });
     }
     //if wrong show error
     //check for duplicate user
